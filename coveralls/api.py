98646917cd325d08eb500d35c1ae897e83f6b87c
Konstantinos Koukopoulos <kouk@transifex.com>
2016-06-09 16:20:36 +0300

    Update api.py

--- coveralls/api.py
+++ coveralls/api.py
@@ -3,13 +3,14 @@ from six import StringIO
 import requests
 
 
-def post(url, repo_token, service_job_id, service_name, git, source_files, parallel):
-    json_file = build_file(repo_token, service_job_id, service_name, git, source_files, parallel)
+def post(url, repo_token, service_number, service_job_id, service_name, git, source_files, parallel):
+    json_file = build_file(repo_token, service_number, service_job_id, service_name, git, source_files, parallel)
     return requests.post(url, files={'json_file': json_file})
 
 
-def build_file(repo_token, service_job_id, service_name, git, source_files, parallel):
+def build_file(repo_token, service_number, service_job_id, service_name, git, source_files, parallel):
     content = {
+        'service_number': service_number,
         'service_job_id': service_job_id,
         'service_name': service_name,
         'git': git,
